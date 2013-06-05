f62ceefa3d129315f1bcea0849c0e034454a7d6d
Artem Shitov <inbox@artemshitov.ru>
2013-06-05 20:55:20 +0400

    Token assignment

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,15 @@
+require 'securerandom'
+
 class Subscription < ActiveRecord::Base
   belongs_to :user
   belongs_to :parcel
+
+  validates :user, uniqueness: { scope: :parcels }
+
+  after_create :assign_token
+
+  def assign_token
+    self.confirmation_token = SecureRandom.urlsafe_base64
+    save
+  end
 end
