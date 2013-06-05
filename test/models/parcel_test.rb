ad467a9ec763cb948095bcb16254f423e8ad45d8
Artem Shitov <inbox@artemshitov.ru>
2013-06-05 21:43:18 +0400

    Test synchronization

--- test/models/parcel_test.rb
+++ test/models/parcel_test.rb
@@ -5,6 +5,20 @@ class ParcelTest < ActiveSupport::TestCase
     @parcel = Fabricate(:parcel)
   end
 
+  def test_syncs
+    assert_equal @parcel.operations.count, 0
+    assert_equal PostOffice.count, 0
+    assert_equal OperationType.count, 0
+
+    VCR.use_cassette(@parcel.barcode) { @parcel.sync }
+
+    assert_operator @parcel.operations.count, :>, 0
+    assert_operator PostOffice.count, :>, 0
+    assert_operator OperationType.count, :>, 0
+
+    assert_operator @parcel.synced_at, :>, DateTime.now - 5
+  end
+
   def test_subscribes_to_parcel_updates
     email = Fabricate.attributes_for(:user)[:email]
     subscription = @parcel.subscribe(email)
