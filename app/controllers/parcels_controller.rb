9c98cf30b19bdc66832b8518bb0485cc9b9b190a
Artem Shitov <inbox@artemshitov.ru>
2013-06-06 01:58:37 +0400

    Parcels representation draft

--- app/controllers/parcels_controller.rb
+++ app/controllers/parcels_controller.rb
@@ -1,10 +1,14 @@
 class ParcelsController < ApplicationController
   def index
+    @parcel = Parcel.new
   end
 
   def show
+    @parcel = Parcel.find_or_create_by(barcode: params[:id])
   end
 
   def create
+    @parcel = Parcel.find_or_create_by(barcode: params[:parcel][:barcode])
+    redirect_to @parcel
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Parcels::Application.routes.draw do
-  get ':barcode' => 'parcels#show', as: :parcel
-  post 'parcels' => 'parcels#create'
+  get '/parcels' => 'parcels#index', as: 'parcels'
+  get '/:id' => 'parcels#show', as: 'parcel'
+  post '/parcels' => 'parcels#create'
   root to: 'parcels#index'
 end
