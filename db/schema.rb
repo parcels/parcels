c2fefe568588e8117f867905428dbf914bddf0ca
Artem Shitov <inbox@artemshitov.ru>
2013-06-05 19:26:58 +0400

    User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130605141138) do
+ActiveRecord::Schema.define(version: 20130605152323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,5 +57,13 @@ ActiveRecord::Schema.define(version: 20130605141138) do
   add_index "post_offices", ["country_code"], name: "index_post_offices_on_country_code", using: :btree
   add_index "post_offices", ["name"], name: "index_post_offices_on_name", using: :btree
   add_index "post_offices", ["postal_code"], name: "index_post_offices_on_postal_code", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "email",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
