bd24ccb4cf939752cc5deba40d448fc87db899ff
Artem Shitov <inbox@artemshitov.ru>
2013-06-13 19:28:07 +0400

    Pending notifications

--- test/models/subscription_test.rb
+++ test/models/subscription_test.rb
@@ -6,6 +6,14 @@ class SubscriptionTest < ActiveSupport::TestCase
   end
 
   def test_assigns_token_on_creation
-    refute_nil @subscription.token  
+    refute_nil @subscription.token
+  end
+
+  def test_pending_notifications
+    assert_empty Subscription.pending_notifications
+    VCR.use_cassette(@subscription.parcel.barcode) do
+      @subscription.parcel.sync
+    end
+    assert Subscription.pending_notifications.include?(@subscription)
   end
 end
