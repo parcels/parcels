55d76b6b4a80800d75cbe07cf20c6464fc578943
Artem Shitov <inbox@artemshitov.ru>
2013-07-15 03:51:44 +0400

    Link instead of form on parcel page

--- app/controllers/parcels_controller.rb
+++ app/controllers/parcels_controller.rb
@@ -7,8 +7,6 @@ class ParcelsController < ApplicationController
 
   def show
     @parcel = Parcel.find_or_create_by(barcode: params[:id].upcase)
-    @new_parcel = Parcel.new(barcode: @parcel.barcode)
-
     @parcel.autosync if @parcel.valid?
   end
 
